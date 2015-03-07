50c38e09356defb2016b382f5ebfc33aa1f48d71
quartz55 <john.razor97@gmail.com>
2015-03-07 11:44:52 +0000

    Commented 4.10 code

--- WebCGF.tp1/tp1/MyUnitCubeQuad.js
+++ WebCGF.tp1/tp1/MyUnitCubeQuad.js
@@ -14,34 +14,40 @@ MyUnitCubeQuad.prototype.constructor=MyUnitCubeQuad;
 
 MyUnitCubeQuad.prototype.display = function (){
 
+    /* Front face */
     this.scene.pushMatrix();
     this.scene.translate(0,0,0.5);
     this.quad.display();
 
+    /* Top face */
     this.scene.popMatrix();
     this.scene.pushMatrix();
     this.scene.translate(0,0.5,0);
     this.scene.rotate(-3.14/2,1,0,0);
     this.quad.display();
 
+    /* Back face */
     this.scene.popMatrix();
     this.scene.pushMatrix();
     this.scene.translate(0,0,-0.5);
     this.scene.rotate(3.14,1,0,0);
     this.quad.display();
 
+    /* Bottom face */
     this.scene.popMatrix();
     this.scene.pushMatrix();
     this.scene.translate(0,-0.5,0);
     this.scene.rotate(3.14/2,1,0,0);
     this.quad.display();
 
+    /* Right face */
     this.scene.popMatrix();
     this.scene.pushMatrix();
     this.scene.translate(0.5,0,0);
     this.scene.rotate(3.14/2,0,1,0);
     this.quad.display();
 
+    /* Left face */
     this.scene.popMatrix();
     this.scene.pushMatrix();
     this.scene.translate(-0.5,0,0);
