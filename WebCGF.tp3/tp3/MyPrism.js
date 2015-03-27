7e84341df327e3c3a68786eaa82d6d0b24412bc0
quartz55 <john.razor97@gmail.com>
2015-03-27 14:55:07 +0000

    Basic Prism done

--- WebCGF.tp3/tp3/MyPrism.js
+++ WebCGF.tp3/tp3/MyPrism.js
@@ -2,46 +2,57 @@
  * MyPrism
  * @constructor
  */
- function MyPrism(scene, slices, stacks) {
- 	CGFobject.call(this,scene);
-	
-	this.slices=slices;
-	this.stacks=stacks;
+function MyPrism(scene, slices, stacks) {
+	  CGFobject.call(this,scene);
 
- 	this.initBuffers();
- };
+	  this.slices=slices;
+	  this.stacks=stacks;
 
- MyPrism.prototype = Object.create(CGFobject.prototype);
- MyPrism.prototype.constructor = MyPrism;
+ 	  this.initBuffers();
+};
 
- MyPrism.prototype.initBuffers = function() {
- 	/*
- 	* TODO:
- 	* Replace the following lines in order to build a prism with a **single mesh**.
- 	*
- 	* How can the vertices, indices and normals arrays be defined to
- 	* build a prism with varying number of slices and stacks?
- 	*/
+MyPrism.prototype = Object.create(CGFobject.prototype);
+MyPrism.prototype.constructor = MyPrism;
 
- 	this.vertices = [
- 	-0.5, -0.5, 0,
- 	0.5, -0.5, 0,
- 	-0.5, 0.5, 0,
- 	0.5, 0.5, 0
- 	];
+MyPrism.prototype.initBuffers = function() {
 
- 	this.indices = [
- 	0, 1, 2, 
- 	3, 2, 1
- 	];
+    this.vertices = [];
+    this.indices = [];
+    // this.normals = [];
 
- 	this.normals = [
- 	0, 0, 1,
- 	0, 0, 1,
- 	0, 0, 1,
- 	0, 0, 1
- 	];
+    const angle = 2*3.14/this.slices; /* 2*PI/nSlices */
 
- 	this.primitiveType = this.scene.gl.TRIANGLES;
- 	this.initGLBuffers();
- };
+    for(i = 0; i < 2*this.slices; i++){
+        this.vertices.push(Math.cos(i*angle));
+        this.vertices.push(Math.sin(i*angle));
+        this.vertices.push(0.5);
+
+        this.vertices.push(Math.cos(i*angle));
+        this.vertices.push(Math.sin(i*angle));
+        this.vertices.push(-0.5);
+
+        // normAngle = i*angle+angle/2;
+        // this.normals.push(Math.cos(normAngle));
+        // this.normals.push(Math.sin(normAngle));
+        // this.normals.push(0);
+
+        // this.normals.push(Math.cos(normAngle));
+        // this.normals.push(Math.sin(normAngle));
+        // this.normals.push(0);
+    }
+
+    for(i = 0; i < this.slices; i++){
+        v = i*2;
+
+        this.indices.push(v);
+        this.indices.push(v+1);
+        this.indices.push(v+2);
+
+        this.indices.push(v+2);
+        this.indices.push(v+1);
+        this.indices.push(v+3);
+    }
+
+ 	  this.primitiveType = this.scene.gl.TRIANGLES;
+ 	  this.initGLBuffers();
+};
